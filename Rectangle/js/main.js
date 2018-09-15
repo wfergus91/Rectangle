3c391d22e9314351e7e5263b56638728247940e6
Will <wfergus@comcast.net>
2018-09-15 14:30:19 -0500

    Initial commit

---
+++ Rectangle/js/main.js
@@ -0,0 +1,12 @@
+﻿function main() {
+	var canvas = document.getElementById('practice');
+	if (!canvas) {
+		console.log('failed to retrieve the <canvas> element');
+		return;
+	}
+
+	var ctx = canvas.msGetInputContext('2d');
+
+	ctx.fillStyle = 'rgba(0,0,255,1.0)';
+	ctx.fillRect(120, 10, 150, 150);
+}
